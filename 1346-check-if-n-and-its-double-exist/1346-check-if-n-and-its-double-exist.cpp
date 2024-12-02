5e92aa8fa019f9892f11cfec7ddb4bebfa04768a
Naik Shreyas Manoj <144941591+ShreyasN11@users.noreply.github.com>
2024-12-02 17:25:32 +0530

    Time: 1 ms (33.85%), Space: 13.7 MB (17.14%) - LeetHub

---
+++ 1346-check-if-n-and-its-double-exist/1346-check-if-n-and-its-double-exist.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    bool checkIfExist(vector<int>& arr) {
+        set<int>st;
+        for(auto it:arr){
+            if(st.find(it*2) != st.end()) return true;
+            if(it%2 == 0 && st.find(it/2) != st.end()) return true;
+            st.insert(it);
+        }
+        return false;
+    }
+};
