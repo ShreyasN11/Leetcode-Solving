4178cd2851bfca479c1de2be4b2fb0b218f65ddb
Naik Shreyas Manoj <144941591+ShreyasN11@users.noreply.github.com>
2024-12-04 12:11:08 +0530

    Time: 5 ms (43.65%), Space: 17.2 MB (7.53%) - LeetHub

--- 2825-make-string-a-subsequence-using-cyclic-increments/2825-make-string-a-subsequence-using-cyclic-increments.cpp
+++ 2825-make-string-a-subsequence-using-cyclic-increments/2825-make-string-a-subsequence-using-cyclic-increments.cpp
@@ -4,10 +4,9 @@ public:
         int i=0; int j=0;
         int n=str1.length(); int m=str2.length();
         while(i<n &&j<m){
-            if(str1[i] == str2[j] || str1[i] + 1 == str2[j] || (str1[i] == 'z' && str2[j] == 'a')){
-                i++; j++;
-            }            
-            else i++;
+            if(str1[i] == str2[j] || str1[i] + 1 == str2[j] || (str1[i] == 'z' && str2[j] == 'a'))
+                j++;
+            i++;
         }
         if(j==m) return true;
         else return false;
