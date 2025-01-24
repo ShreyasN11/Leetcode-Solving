2d37b0157e54857a3fd86b4e35dcf0ca23fb9493
Naik Shreyas Manoj <144941591+ShreyasN11@users.noreply.github.com>
2025-01-24 14:13:32 +0530

    Time: 0 ms (100%), Space: 12.6 MB (34.1%) - LeetHub

--- 1769-minimum-number-of-operations-to-move-all-balls-to-each-box/1769-minimum-number-of-operations-to-move-all-balls-to-each-box.cpp
+++ 1769-minimum-number-of-operations-to-move-all-balls-to-each-box/1769-minimum-number-of-operations-to-move-all-balls-to-each-box.cpp
@@ -3,14 +3,35 @@ public:
     vector<int> minOperations(string boxes) {
         int n= boxes.size();
         vector<int>res(n,0);
+        vector<int>l(n,0);
+        vector<int>r(n,0);
+
+        int count = boxes[0] == '1' ? 1 : 0;
+
+        for(int i=1; i<n; i++){
+            l[i] = l[i-1] + count;
+            count += boxes[i] == '1' ? 1 : 0;
+        }
+
+        count = boxes[n-1] == '1' ? 1 : 0;
+
+        for(int i=n-2; i>=0; i--){
+            r[i] = r[i+1] + count;
+            count += boxes[i] == '1' ? 1 : 0;
+        }
 
         for(int i=0; i<n; i++){
-            for(int j=0; j<n; j++){
-                int x = boxes[j] == '1' ? 1 : 0;
-                res[i] += x*abs(i-j);
-            }
+            res[i] = r[i] + l[i];
         }
+
+        return res;        
 
         return res;
     }
 };
+
+/*
+
+    cost of getting all balls to adjacent + number of ball
+
+*/
