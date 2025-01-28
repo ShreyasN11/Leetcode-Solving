c57aab4cbf20765ffc09eb072bf1f6e46c2a81f7
Naik Shreyas Manoj <144941591+ShreyasN11@users.noreply.github.com>
2025-01-28 16:52:23 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 2658-maximum-number-of-fish-in-a-grid/2658-maximum-number-of-fish-in-a-grid.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    int dfs(int i, int j, vector<vector<int>>& grid, vector<vector<int>> &visited){
+        int drow[] = {0,1,0,-1};
+        int dcol[] = {-1,0,1,0};
+        int sum = 0;
+        int n = grid.size(); int m = grid[0].size();
+        for(int k =0; k<4; k++){
+            int ni = i + drow[k];
+            int nj = j + dcol[k];
+            if(ni>=0 && ni<n && nj>=0 && nj<m){
+                if(grid[ni][nj] && ! visited[ni][nj]){
+                    visited[ni][nj] = 1;
+                    sum += dfs(ni,nj,grid,visited);
+                }
+            }
+        }
+        return sum + grid[i][j];
+    }
+    int findMaxFish(vector<vector<int>>& grid) {
+        int n = grid.size();
+        int m = grid[0].size();
+        vector<vector<int>> visited(n, vector<int>(n,0));
+        int maxi =0;
+        for(int i=0; i<n; i++){
+            for(int j=0; j<m; j++){
+                if(!visited[i][j] && grid[i][j]){
+                    visited[i][j] = 1;
+                    int sum = dfs(i,j,grid,visited);
+                    maxi = max(sum,maxi);
+                }
+            }
+        }
+        return maxi;
+    }
+};
